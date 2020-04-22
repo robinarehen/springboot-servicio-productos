aaef13fcd038bd765b5029e6e38864a06f83fedd
Robin Arellano <robinarehen@gmail.com>
2020-04-22 10:59:15 +0200

    refactor: se implementa [spring-cloud-starter-netflix-eureka-client] y se configura el proyecto para que se registre ante [spring-cloud-starter-netflix-eureka-server]

--- src/main/java/com/rahdevelopers/api/productos/apicontroller/ProductoApiController.java
+++ src/main/java/com/rahdevelopers/api/productos/apicontroller/ProductoApiController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rahdevelopers.api.productos.entity.ProductoEntity;
 import com.rahdevelopers.api.productos.service.ProductoService;
 
 @RestController
+@RequestMapping("/producto")
 public class ProductoApiController {
 
 	@Autowired
@@ -21,7 +23,7 @@ public class ProductoApiController {
 		return this.productoService.findAll();
 	}
 
-	@GetMapping("/ver/{id}")
+	@GetMapping("/{id}")
 	public ProductoEntity getProducto(@PathVariable Long id) {
 		return this.productoService.findById(id);
 	}

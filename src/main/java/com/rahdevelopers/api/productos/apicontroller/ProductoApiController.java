69e99b75c1e45859aa5792f10712ac8dcdacdbb2
Robin Arellano <robinarehen@gmail.com>
2020-04-22 15:24:15 +0200

    refactor: se configura el properties para que el puerto sea auto generado, el nombre de la ruta se manajera mediante el proxy

--- src/main/java/com/rahdevelopers/api/productos/apicontroller/ProductoApiController.java
+++ src/main/java/com/rahdevelopers/api/productos/apicontroller/ProductoApiController.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rahdevelopers.api.productos.entity.ProductoEntity;
 import com.rahdevelopers.api.productos.service.ProductoService;
 
 @RestController
-@RequestMapping("/producto")
 public class ProductoApiController {
 
 	@Autowired
